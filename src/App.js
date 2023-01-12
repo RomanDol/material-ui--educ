33852d7ac4ea0f276833ed9d486d5c41346e1284
RomanDol <dolinovskyi@gmail.com>
2023-01-12 23:53:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { AppBar, Button, Toolbar } from "@mui/material"
+import { Container } from "@mui/system"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppBar position="fixed">
+      <Container>
+        <Toolbar>
+          
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
 }
 
-export default App;
+export default App
